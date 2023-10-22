154a849008a247801dee933b25b969786b35aeeb
Aklimaakter100 <146802307+Aklimaakter100@users.noreply.github.com>
2023-10-22 13:17:56 +0600

    Add files via upload

---
+++ exam.js
@@ -0,0 +1,121 @@
+// Ans:1
+//JS Statement: var number = 10;
+
+// Ans:2
+//1..1st e capital letter use kora jabe na.
+//2..Word er modde space use kora jabe na.
+//3..1st e number use kora jabe na.
+//4..if,else,true,etc use kora jabe na.
+//5..special character use kora jabe na.
+
+// Ans :3
+//some name of datatype: integer(1.2.3),float(11.3, 1.8),string("Aklima").
+
+// Ans:4
+// variable namkoroner jonno nicher bishoy gulo kheal korte hobe:
+//1..1st e capital letter use kora jabe na.
+//2..Word er modde space use kora jabe na.
+//3..1st e number use kora jabe na.
+//4..if,else,true,etc use kora jabe na.
+//5..special character use kora jabe na.
+
+// // Ans: 5
+// var my_name = "Aklima";
+// console.log(my_name);
+
+// Ans: 6
+//Ganitik kaj somponno kore.
+
+// Ans: 7
+//% = Vagses ber kore.
+//** = kono shongkhar power ber kore.
+// / = vag kore.
+
+// Ans: 8
+// ja diye operator er kaj kora hoy tai operand.Example--a,b,x,,y etc.
+
+// Ans : 9
+//var x = 10;
+// //var y = 36;
+// //var z = x + y;
+// console.log(z);
+
+// Ans: 10
+// var x = 375;
+// var y = 8;
+// var a = x/y;
+// console.log(a);
+
+// Ans: 11
+// var num = 8;
+// var power = 3;
+// var power_num = num**power;
+// console.log(power_num);
+
+//Ans: 12
+// var mat = 80;
+// var che = 85;
+// var phy =70;
+// var bang =80;
+// var all = mat + che + phy + bang;
+// var avg = all/ 4;
+// console.log(avg);
+
+// Ans: 13
+// var x = 13;
+// var Y = 23;
+// var z = x+Y;
+// console.log(z);
+
+// Ans : 14
+// type of diye number ti ki type er ta ber kora hoy.
+// var number = 10.2;
+// console.log();
+
+// Ans 16:
+// 0 theke start history.
+
+//Ans: 17
+// var numbers = [34,56,74,24,88,34,776,34,5];
+// numbers [5]=334;
+// console.log(numbers);
+
+// Ans : 18
+// var numbers = [34,56,74,24,88,34,776,34,5];
+// numbers.push(234);
+// console.log(numbers);
+
+// var numbers = [34,56,74,24,88,34,776,34,5];
+// numbers.shift(998);
+// console.log(numbers);
+
+// Ans : 25
+// var total_price = 185;
+// var total_pen = 10;
+// var each_pen = total_price/total_pen;
+// var result = each_pen*3;
+// console.log(result);
+
+// Ans: 19
+// var numbers = [[1,2],[3,4],[5,6]];
+// console.log(numbers);
+// var new_numbers=flat(numbers);
+// console.log(new_numbers);
+
+// Ans: 20
+// // var team1=["rahim","karim"];
+// var team2=["tanim","biplop","tareq"];
+// var team3=["sajjad","arefin"];
+// var teams = team1 + team2 +team3;
+// consolel.log(teams);
+
+// Ans: 26
+// var main_num = 20;
+// var percent = 7;
+// var result = main_num*percent/100;
+// console.log(result);
+
+
+Ans : 30
+// Routine onojai prottekdiner kaj gulo complete korar maddhome ami amar somoyer best use 
+//korte parbo.
